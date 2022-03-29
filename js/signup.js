a019e9389f053cedf050cc7d5af3f5c43a984e0b
prabhash1475 <99678086+prabhash1475@users.noreply.github.com>
2022-03-29 16:32:45 +0530

    signup

---
+++ js/signup.js
@@ -0,0 +1,16 @@
+document.querySelector("form").addEventListener("submit", signupfun);
+
+var userdata = JSON.parse(localStorage.getItem("userCred")) || [];
+
+function signupfun() {
+  event.preventDefault();
+
+  var userobj = {
+    name1: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    pass: document.querySelector("#pass").value,
+  };
+  userdata.push(userobj);
+  //   console.log(userdata);
+  localStorage.setItem("userCred", JSON.stringify(userdata));
+}
