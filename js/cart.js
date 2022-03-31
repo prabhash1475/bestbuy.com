7304f998d0a30bbcff8720ff22cc8ab7ecce5a0e
prabhash1475 <99678086+prabhash1475@users.noreply.github.com>
2022-03-31 15:58:21 +0530

    cart page updated

--- js/cart.js
+++ js/cart.js
@@ -4,28 +4,41 @@ var total = cartData.reduce(function (sum, elem, index, arr) {
   return sum + Number(elem.price);
 }, 0);
 var sum = document.createElement("h2");
-sum.innerText = `Total is Rs = ${total}`;
+sum.innerText = "Cart Iteams";
+
+var length = cartData.length;
+console.log(length);
+var length_cart = document.querySelector(".length");
+length_cart.innerText = `Total No of product is - ${length}`;
+
+var all = document.querySelector(".total_price");
+all.innerText = `Total is Rs = ${total}`;
 document.querySelector(".contaner").append(sum);
 
 cartData.map(function (elem, index) {
   var box = document.createElement("div");
+  box.setAttribute("class", "box");
 
   var img = document.createElement("img");
   img.src = elem.image_url;
 
+  var first = document.createElement("div");
+  first.append(img);
   var p = document.createElement("p");
   p.innerText = elem.name;
 
-  var price = document.createElement("p");
+  var price = document.createElement("h3");
   price.innerText = elem.price;
 
   btn = document.createElement("button");
   btn.innerText = "Remove";
   btn.addEventListener("click", function () {
-    removeIteam(elem, index);
+    removeIteam(first, index);
   });
 
-  box.append(img, p, price, btn);
+  var second = document.createElement("div");
+  second.append(p, price, btn);
+  box.append(img, second);
   document.querySelector(".contaner").append(box);
 });
 
